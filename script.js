e5de9e8745726273fe5a1531acf5f65d70728b11
NoNameSVK123 <nonamesvk123@gmail.com>
2025-02-19 18:28:04 +0100

    Add UI support

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rockButton = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const buttons = document.querySelector(".buttons");
+const score = document.querySelector(".score");
+const results = document.querySelector(".results");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice;
@@ -16,40 +24,55 @@ function getHumanChoice() {
     return prompt("Choose one: Rock, Paper, Scissors").toLowerCase();
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+function updateScore(){
+    score.textContent = `${humanScore}:${computerScore}`;
+}
 
-    function playRound(humanChoice=getHumanChoice(), computerChoice=getComputerChoice()) {
+function playRound(humanChoice=getHumanChoice(), computerChoice=getComputerChoice()) {
+    if (humanScore<5 && computerScore<5){
         humanChoice = humanChoice.toLowerCase();
         computerChoice = computerChoice.toLowerCase();
         if (humanChoice===computerChoice) {
-            console.log("Draw");
+            results.textContent= "Draw";
         }else{
             //human wins
             if (humanChoice==="rock" && computerChoice=="scissors" ||
                 humanChoice==="scissors" && computerChoice=="paper" ||
                 humanChoice==="paper" && computerChoice=="rock") {
                     humanScore++;
-                    console.log("You won the round!");
+                    results.textContent= "You won the round!";
                 }else{
                     computerScore++;
-                    console.log("You lost the round!");
+                    results.textContent= "You lost the round!";
                 }
         }
-    }
-
-    for (let i=0; i<5; i++) {
-        playRound();
-    }
-
-    if (humanScore==computerScore) {
-        console.log("It is a draw! Final score: " + humanScore + ":" + computerScore);  
-    }else if (humanScore>computerScore) {
-        console.log("You won the game! Final score: " + humanScore + ":" + computerScore);  
-    }else{
-        console.log("You lost the game! Final score: " + computerScore + ":" + humanScore);  
+        updateScore();
+        if (computerScore==5 || humanScore==5){
+            if (humanScore==computerScore) {
+                results.textContent= "It is a draw! Final score: " + humanScore + ":" + computerScore;
+            }else if (humanScore>computerScore) {
+                results.textContent= "You won the game! Final score: " + humanScore + ":" + computerScore;
+            }else{
+                results.textContent= "You lost the game! Final score: " + computerScore + ":" + humanScore;
+            }
+        }
     }
 }
 
-playGame()
+buttons.addEventListener("click",(e) =>{
+    let target = e.target;
+    
+
+    switch (target.getAttribute("class")){
+        case 'rock':
+            playRound("rock");
+            break;
+        case 'paper':
+            playRound("paper");
+            break;
+        case 'scissors':
+            playRound("scissors");
+            break; 
+    }
+});
+
